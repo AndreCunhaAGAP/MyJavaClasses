a69ba798081833686aada7698df340eaaee5fc7b
AndreCunha <AndreCunha>
2016-01-07 21:45:11 +0000

    fixed error

--- Aula001/com/agap2/training/javaclasses/App.java
+++ Aula001/com/agap2/training/javaclasses/App.java
@@ -5,6 +5,8 @@ public class App {
 	public static void main(String[] args){
 		System.out.println("Hello World!");
 		
-		System.out.println("Hellow Java!");
+		System.out.println("Hello Java!");
+		
+		System.out.println("Hello André!");
 	}
 }
